4a1ebdb9f250594644f1760da69aa00638b0b798
Manzi-serge <manziserge568@gmail.com>
2024-08-05 06:08:05 -0700

    ft-registration-form

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,21 @@ function App() {
   return (
     <>
       <div className='main-container'>
-        <h1>List</h1>
-        <ul className="list-group">
-          <li className="list-group-items">An item</li>
-          <li className="list-group-items">A conclusion</li>
-          <li className="list-group items">A body</li>
-        </ul>
+      <div class="container-items">
+        <span class="page-title"> Welcome back.</span>
+        <div class="page-form">
+            <label for="email">Email/address</label>
+          <input type="text" id="email" placeholder="info Fuko.com" />
+            <label for="password">Password</label>
+          <input type="password" id="password" placeholder="Enter password" />
+          <a href="#" class="forgot-pwd-btn">Forgot password?</a>
+          <a href="#" onclick="login()" class="sign-in-btn">Sign in</a>
+          <div class="signup-btn">
+            <span>Don't have an acount </span>
+            <a href="#">sign up now</a>
+          </div>
+        </div>
+      </div>
       </div>
     </>
   )
